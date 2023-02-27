923fac6ab6a5e2c1cf4c6b787d55adad19334274
xan2301 <2silverghost2@gmail.com>
2023-02-27 23:28:00 +0100

    add Print Integer in java

---
+++ src/main/java/org/example/programs/Integers.java
@@ -0,0 +1,18 @@
+package org.example.programs;
+
+public class Integers {
+
+    public static void main(String[] arg) {
+
+
+        int c; //declaring a variable
+        /* Using for loop to repeat instruction execution */
+
+        for (c = 1; c <= 100; c++) {
+            System.out.println(c);
+        }
+    }
+}
+
+
+
