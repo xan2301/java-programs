05e0d912a1c0c36d943756aa223ebc5d67487f45
xan2301 <2silverghost2@gmail.com>
2023-03-03 16:32:30 +0100

    day4

---
+++ src/main/java/org/example/programs/day4/Factorial.java
@@ -0,0 +1,14 @@
+package org.example.programs.day4;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        int n, c, fact = 1;
+
+        System.out.println("Enter an integer to calculate it's factorial");
+
+
+    }
+
+}
