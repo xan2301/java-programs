8476805118b44fcc8ecae41f0d8b857e5bb6a7f8
xan2301 <2silverghost2@gmail.com>
2023-03-08 19:39:41 +0100

    day4

---
+++ src/main/java/org/example/programs/day4/BigFactorial.java
@@ -0,0 +1,33 @@
+package org.example.programs.day4;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial {
+
+    public static void main(String[] args) {
+
+        int n, c;
+
+        BigInteger inc = new BigInteger("1");
+        BigInteger fact = new BigInteger("1");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input an integer: ");
+
+        n = input.nextInt();
+
+        for (c = 1; c <= n; c++) {
+
+            fact = fact.multiply(inc);
+            inc = inc.add(BigInteger.ONE);
+
+
+        }
+
+        System.out.println( n + "! =" + fact);
+
+
+    }
+}
---
+++ src/main/java/org/example/programs/day4/CompareStrings.java
@@ -0,0 +1,28 @@
+package org.example.programs.day4;
+
+import java.util.Scanner;
+
+public interface CompareStrings {
+
+    public static void main(String[] args) {
+
+        String s1, s2;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(" Enter first String: ");
+
+        s1 = in.nextLine();
+
+        System.out.println(" Enter first String: ");
+
+        s2 = in.nextLine();
+
+
+
+
+
+    }
+
+
+}
